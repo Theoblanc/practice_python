77e3fe9a3c9a9ed1bb5ed990549111d932037889
TheoBlanc <jtho1015@gmail.com>
2020-07-14 00:40:18 +0900

    :star: new: study quick sort

---
+++ sort/quick.py
@@ -0,0 +1,37 @@
+import random
+
+
+class QuickSort(object):
+    def __init__(self, numbers):
+        self.values = numbers
+        self.count = len(self.values)
+
+    def sort(self):
+        self.quick_sort(0, self.count - 1)
+        return self.values
+
+    def quick_sort(self, left, right):
+        i = left
+        j = right
+
+        pivot_index = random.randint(left, right)
+        pivot = self.values[pivot_index]
+
+        while i < j:
+            while self.values[i] < pivot:
+                i += 1
+            while self.values[j] > pivot:
+                j -= 1
+            if i <= j:
+                self.values[i], self.values[j] = self.values[j], self.values[i]
+                i += 1
+                j -= 1
+
+        if left < j:
+            self.quick_sort(left, j)
+        if right > i:
+            self.quick_sort(i, right)
+
+
+c = QuickSort([9, 3, 7, 1, 6, 4])
+print(c.sort())
